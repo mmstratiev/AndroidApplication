b5b77f25b2a79733cf186ebd701a17bf4ceb37f6
mmstratiev <mmstratiev@gmail.com>
2018-02-17 21:42:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/gallo/appproject/LoginRequest.java
@@ -0,0 +1,29 @@
+package com.example.gallo.appproject;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Gallo on 1/19/2018.
+ */
+
+public class LoginRequest extends StringRequest {
+    private static final String REGISTER_REQUEST_URL = "https://dissonant-curl.000webhostapp.com/Login.php";
+    private Map<String, String> params;
+
+    public LoginRequest(String username, String password, Response.Listener<String> listener){
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+
+        params = new HashMap<>();
+        params.put("username", username);
+        params.put("password", password);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
